46e2bda9f2efc0736e90f1bee8a95f32aad60342
mati <mateuszzaniewski1@gmail.com>
2025-01-07 20:17:52 +0100

    Add: styled component for Hero section

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -17,6 +17,79 @@ const HeroContainer = styled.div`
     z-index: 1;
     clip-path: polygon(0 0, 100% 0, 100% 100%, 70% 95% ,0 100%);
 `;
+
+const HeroBg = styled.div`
+    position: absolute;
+    display: flex;
+    justify-content: end;
+    top: 50%;
+    right: 0;
+    bottom: 0;
+    left: 50%;
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+    padding: 0 30px;
+    -webkit-transform: translateX(-50%) translateY(-50%);
+    transform: translateX(-50%) translateY(-50%);
+    
+    @media screen and (max-width: 960px) {
+        padding: 0;
+        justify-content: center;
+    }
+`;
+
+const HeroInnerContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    max-width: 1100px;
+    
+    @media screen and (max-width: 960px) {
+        flex-direction: column;
+    }
+`;
+
+const HeroLeftContainer = styled.div`
+    width: 100%;
+    order: 1;
+    @media screen and (max-width: 960px) {
+        order: 2;
+        margin-right: 32px;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+    
+    @media screen and (max-width: 640px) {
+        order: 2;
+        margin-bottom: 32px;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+`;
+
+const HeroRightContainer = styled.div`
+    display: flex;
+    justify-content: end;
+    gap: 12px;
+    width: 100%;
+    order: 2;
+    @media screen and (max-width: 960px) {
+        order: 1;
+        margin-bottom: 80px;
+        justify-content: center;
+        align-items: center;
+    }
+    
+    @media screen and (max-width: 640px) {
+        order: 1;
+        margin-bottom: 30px;
+    }
+
+`;
 function Hero(props) {
     return (
         <div id = "about">
