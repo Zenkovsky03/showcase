423918ac369c19cbf128bdb5d5cc29927e02f3dc
mati <mateuszzaniewski1@gmail.com>
2025-01-09 23:39:40 +0100

    Finish: Skills section with styled component and rendering data from const file

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -46,6 +46,78 @@ const Desc = styled.div`
     }
 `;
 
+const SkillsContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 30px;
+    justify-content: center;
+    gap: 30px;
+`;
+
+const Skill = styled.div`
+    width: 100%;
+    max-width: 500px;
+    background-color: ${({ theme }) => theme.card};
+    border: 0.1px solid #854CE6;
+    border-radius: 16px;
+    padding: 18px 36px;
+    
+    @media (max-width: 768px){
+        max-width: 400px;
+        padding: 10px 36px;
+    }
+    
+    @media (max-width: 500px){
+        max-width: 330px;
+        padding: 10px 36px;
+    }
+`;
+
+const SkillTitle = styled.h2`
+    font-size: 28px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.text_secondary};
+    margin-bottom: 20px;
+    text-align: center;
+`;
+
+const SkillList = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 12px;
+    margin-bottom: 20px;
+`;
+
+const SkillItem = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    justify-content: center;
+    font-size: 16px;
+    color: ${({ theme }) => theme.text_primary+80};
+    border-radius: 12px;
+    border: 1px solid ${({ theme }) => theme.text_primary+80};
+    padding: 12px 16px;
+    font-weight: 400;
+    
+    @media (max-width: 768px){
+        font-size: 14px;
+        padding: 8px 12px;
+    }
+    
+    @media (max-width: 500px){
+        font-size: 14px;
+        padding: 6px 12px;
+    }
+`;
+
+const SkillImage = styled.img`
+    width: 24px;
+    height: 24px;
+`;
+
 
 function Skills(props) {
     return (
@@ -53,21 +125,21 @@ function Skills(props) {
             <Wrapper>
                 <Title>Skills</Title>
                 <Desc>Here are some of my skills </Desc>
-                {/*<SkillsContainer>*/}
-                {/*    {skills.map((item) =>(*/}
-                {/*        <Skill>*/}
-                {/*            <SkillTitle>{item.title}</SkillTitle>*/}
-                {/*            <SkillList>*/}
-                {/*                {item.skills.map((skill) => (*/}
-                {/*                    <SkillItem>*/}
-                {/*                        <SkillImage src={skill.image} />*/}
-                {/*                        {skill.name}*/}
-                {/*                    </SkillItem>*/}
-                {/*                ))}*/}
-                {/*            </SkillList>*/}
-                {/*        </Skill>*/}
-                {/*    ))}*/}
-                {/*</SkillsContainer>*/}
+                <SkillsContainer>
+                    {skills.map((item) =>(
+                        <Skill>
+                            <SkillTitle>{item.title}</SkillTitle>
+                            <SkillList>
+                                {item.skills.map((skill) => (
+                                    <SkillItem>
+                                        <SkillImage src={skill.image} />
+                                        {skill.name}
+                                    </SkillItem>
+                                ))}
+                            </SkillList>
+                        </Skill>
+                    ))}
+                </SkillsContainer>
             </Wrapper>
         </Container>
     );
